89dfb4df29bf7cfbc6896113a529ef6039e5400a
v_luzhaoyang <v_luzhaoyang@tal.com>
2025-05-20 17:33:28 +0800

    第一次尝试部署

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="我的第一个vue的界面处理" />
 </template>
 
 <style scoped>
